4ea5fa35a2bbba587f3bed3f8d79c953db278290
sultan-kasim <sultan.kasim@stu.ocsb.ca>
2025-10-17 12:52:02 -0400

    Shows all colours

--- main.ts
+++ main.ts
@@ -1,8 +1,60 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2025 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Sultan
+ * Created on: Sep 2025
+ * This program shows all colors
 */
 
-basic.showString('Hello, World!')
+// cleanup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// shows all colors on A button
+input.onButtonPressed(Button.A, function () {
+    // turns on red pin
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+
+    // turns on blue pin
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+
+    // turns on green pin
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+
+    // turns on blue and red pin
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+
+    // turns on green and red pin
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+
+    // turns on green and blue pin
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+
+    // turns on green, red and blue pin
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+})
+
